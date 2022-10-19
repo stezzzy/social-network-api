f7623d2295ecd052db2fe1c9af2c9b15e3d77120
Nick Stevens <90112060+stezzzy@users.noreply.github.com>
2022-10-19 02:29:35 -0400

    saving user controller

---
+++ controllers/userController.js
@@ -0,0 +1,122 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+// Aggregate function for getting the overall grade using $avg
+const grade = async (userId) =>
+  User.aggregate([
+    // only include the given user by using $match
+    { $match: { _id: ObjectId(userId) } },
+    {
+      $unwind: "$reactions",
+    },
+    {
+      $group: {
+        _id: ObjectId(userId),
+        overallGrade: { $avg: "$reactions.score" },
+      },
+    },
+  ]);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json({
+              user,
+              grade: await grade(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+  // Delete a user and remove them from the thought
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) => {
+        if (!user) {
+          res.status(404).json({ message: "No such user exists" });
+        }
+      })
+      .then(() =>
+          res.json({ message: "User successfully deleted" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add an friend to a user
+  addFriend(req, res) {
+    console.log("You are adding an friend");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove friend from a user
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId  } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
