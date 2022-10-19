aa6cd24a42275b1d6ea3004ba8555377a972e85f
Nick Stevens <90112060+stezzzy@users.noreply.github.com>
2022-10-19 02:23:25 -0400

    saving routes progress

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+  } = require('../../controllers/thoughtController.js');
+
+
+  // api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// api/thoughts/:thoughtId/
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions/').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
+
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
-
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser)
